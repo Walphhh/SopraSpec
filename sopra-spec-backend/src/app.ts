6a7a48b015ce02eb3603d0115c4594003eaf80b6
Walphhh <93171385+Walphhh@users.noreply.github.com>
2025-08-14 18:55:24 +1000

    added morgan middleware to log requests

--- sopra-spec-backend/src/app.ts
+++ sopra-spec-backend/src/app.ts
@@ -1,7 +1,11 @@
 import express from "express";
 import cors from "cors";
+import morgan from "morgan";
 
 const app = express();
+
+// Middleware to log request
+app.use(morgan("dev"));
 
 app.use(cors());
 app.use(express.json());
